8d90c27797a0f94372d8799268ba205da9f51c5b
Katerina Mouzakiti <mouzakiti.katerina@nbg.gr>
2021-05-19 13:03:28 +0300

    τεστ

---
+++ src/main/java/com/example/katemovies/KatemoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.katemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatemoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatemoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/katemovies/KatemoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katemovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatemoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
